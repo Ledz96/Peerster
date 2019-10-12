01819d0bbc430df12a94c7ec65e62b71cb8acb61
Ledz96 <alessandro.bianchi003@gmail.com>
2019-10-12 15:19:01 +0200

    Adding debug messages

--- gossiper/gossiper.go
+++ gossiper/gossiper.go
@@ -545,7 +545,7 @@ func (g gossiper) isMongeringDone(c chan *gossippacket.GossipPacket, peer string
 	case gp := <-c:
 		if gp.Status == nil {
 			fmt.Println("ERROR: NOT STATUS PACKET SENT IN STATUS CHANNEL?!")
-			fmt.Println(gp)
+			fmt.Println(gp.Rumor.Text)
 			os.Exit(-1)
 		}
 		s := g.compareStatus(*gp.Status, peer)
